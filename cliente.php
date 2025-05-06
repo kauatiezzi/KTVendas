d192006a0d5545cd9cdc786f8527f958f4cd947f
kauatiezzi <kauatiezzi02@hotmail.com>
2025-05-06 14:25:21 -0300

    adicionado favicon

--- cliente.php
+++ cliente.php
@@ -21,7 +21,11 @@ if (!empty($_GET['bonus'])) {
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
-    <title>Dashboard</title>
+    <title>Clientes - Doceria Gourmet</title>
+    <link rel="apple-touch-icon" sizes="180x180" href=".\assets\imagens\apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href=".\assets\imagens\favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href=".\assets\imagens\favicon-16x16.png">
+    <link rel="manifest" href=".\assets\imagens\site.webmanifest">
     <script src="https://cdn.tailwindcss.com"></script>
     <style>
         /* Paleta de cores de doceria */
